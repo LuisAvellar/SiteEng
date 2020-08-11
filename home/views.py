1b80b2b5c957dee63485fcb47eaab0983d842e1d
luisAvellar <luisaugustoeq@gmail.com>
2020-08-11 09:13:09 -0300

    Second Commit

--- home/views.py
+++ home/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+# def index(request):
+#     return render(request, "home/index.html")
+    # return HttpResponse("Hello, User!")
+    
+
+# def index(request, name):
+#     now= datetime.datetime.now()
+#     return render(request, "home/index.html", {
+#         "name": name.capitalize(),
+#         "date": now.month == 8 and now.day == 9,
+#     })
+
+def index(request):
+
+    return render(request, "home/index.html")
+
+def index1(request):
+    
+    return render(request, "home/reusable.html")
