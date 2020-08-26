2c3492fa7585a5f5cd6bd711bdd5116f5da17539
luisAvellar <luisaugustoeq@gmail.com>
2020-08-26 15:14:13 -0300

    Starting page contents

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django  import template
 
 # def index(request):
 #     return render(request, "home/index.html")
@@ -14,10 +14,21 @@ from django.shortcuts import render
 #         "date": now.month == 8 and now.day == 9,
 #     })
 
+loop_times = range(1, 8)
+z=["z","zz","sss","ddd","f","f","f","f","f","f","f"]
+
+a=zip(loop_times, z)
+a=loop_times
+
+
 def index(request):
 
-    return render(request, "home/index.html")
+    return render(request, "home/ContentUnitConvertion.html.html")
 
 def index1(request):
+
+    return render(request, "home/ContentHome.html", { "loop_times" : loop_times, "z" : z, "a" : a  })
+
+def index2(request):
     
     return render(request, "home/reusable.html")
